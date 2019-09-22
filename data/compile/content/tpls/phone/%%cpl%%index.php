926f5af1f447947ef3cecb6cb2dba0b552fd2133
root <1009464715@qq.com>
2019-09-22 20:05:40 +0800

    11

---
+++ data/compile/content/tpls/phone/%%cpl%%index.php
@@ -0,0 +1,172 @@
+<?php if(!$this->tpl_var['userhash']){ ?>
+<?php $this->_compileInclude('header'); ?>
+<?php $signPackage = $this->G->make('jssdk','user')->GetSignPackage();; ?>
+<body>
+<div id="content">
+    <div class="pages" id="page1">
+<?php } ?>
+    	<div class="pagecontent">
+		<header class="container-fluid" style="background-color:#337AB7;">
+			<h5 class="text-center">
+				<em style="font-size:2rem;" class="pull-left glyphicon glyphicon-home"></em>
+				<span class="ttlo">PHPEMS模拟考试系统</span>
+				<a style="font-size:2rem;color:#FFFFFF;" href="index.php?user-phone" class="pull-right glyphicon glyphicon-user ajax" data-target="user" data-page="user"></a>
+			</h5>
+		</header>
+    	<div class="container-fluid" style="overflow:hidden;margin-top:1rem;margin-bottom:0.4rem;">
+			<div class="swiper-container">
+	    		<div class="swiper-wrapper">
+		    		<?php $cid = 0;
+ foreach($this->tpl_var['contents'][2]['data'] as $key => $content){ 
+ $cid++; ?>
+		    		<div class="swiper-slide">
+		    			<a href="index.php?content-phone-content&contentid=<?php echo $content['contentid']; ?>" class="ajax" data-page="rollpage" data-target="rollpage">
+		    				<img src="<?php echo $content['contentthumb']; ?>">
+		    			</a>
+		    		</div>
+		    		<?php } ?>
+	    		</div>
+	    		<div class="swiper-pagination"></div>
+	    	</div>
+		</div>
+		<div class="container-fluid">
+			<div style="clear:both;overflow:hidden;" class="items">
+				<div class="col-xs-3" style="padding:0.2rem;">
+					<div class="text-center" style="padding:0.2rem;padding-top:1.5rem;">
+						<a href="index.php?exam-phone-index" class="ajax" data-page="mybasic" data-target="mybasic">
+							<p>
+								<img src="app/core/styles/img/exam.png" style="width:92%;"/>
+							</p>
+							<h5>在线考试</h5>
+						</a>
+					</div>
+				</div>
+				<div class="col-xs-3" style="padding:0.2rem;">
+					<div class="text-center" style="padding:0.2rem;padding-top:1.5rem;">
+						<a class="ajax" href="index.php?course-phone-index" data-target="course" data-page="course">
+							<p>
+								<img src="app/core/styles/img/video.png" style="width:92%;"/>
+							</p>
+							<h5>视频课程</h5>
+						</a>
+					</div>
+				</div>
+				<div class="col-xs-3" style="padding:0.2rem;">
+					<div class="text-center" style="padding:0.2rem;padding-top:1.5rem;">
+						<a href="index.php?certificate-phone" data-target="certificate" data-page="certificate" class="ajax">
+							<p>
+								<img src="app/core/styles/img/t3.png" style="width:92%;"/>
+							</p>
+							<h5>证书申请</h5>
+						</a>
+					</div>
+				</div>
+				<div class="col-xs-3" style="padding:0.2rem;">
+					<div class="text-center" style="padding:0.2rem;padding-top:1.5rem;">
+						<a href="index.php?content-phone-category" data-target="contentcategory" data-page="contentcategory" class="ajax">
+							<p>
+								<img src="app/core/styles/img/news.png" style="width:92%;"/>
+							</p>
+							<h5>考试资讯</h5>
+						</a>
+					</div>
+				</div>
+			</div>
+			<h4 style="overflow:hidden;clear:both;padding-top:0.2rem;">
+				<span class="pull-left" style="width:35%"><hr /></span>
+				<span class="col-xs-4 text-center" style="width:30%;line-height:4rem;">热门考场</span>
+				<span class="pull-right" style="width:35%"><hr /></span>
+			</h4>
+			<div style="clear:both;padding:0px;margin-bottom:1rem;overflow: hidden;">
+				<?php $bid = 0;
+ foreach($this->tpl_var['basics']['basic'] as $key => $basic){ 
+ $bid++; ?>
+				<div class="col-xs-6" style="padding:3px;">
+					<div style="background-color:#FFFFFF;padding:7px">
+						<a href="index.php?exam-phone-index-setCurrentBasic&basicid=<?php echo $basic['basicid']; ?>" class="ajax" data-target="basic" data-page="basic">
+							<img src="<?php echo $basic['basicthumb']; ?>" style="width:100%;"/>
+						</a>
+						<h5 class="text-center">
+							<a href="index.php?exam-phone-index-setCurrentBasic&basicid=<?php echo $basic['basicid']; ?>" class="ajax" data-target="basic" data-page="basic"><?php echo $basic['basic']; ?></a>
+						</h5>
+					</div>
+				</div>
+				<?php } ?>
+			</div>
+			<div>
+				<h4 style="overflow:hidden;clear:both;padding-top:0.2rem;">
+					<span class="pull-left" style="width:35%"><hr /></span>
+					<span class="col-xs-4 text-center" style="width:30%;line-height:4rem;">视频课程</span>
+					<span class="pull-right" style="width:35%"><hr /></span>
+				</h4>
+			</div>
+			<div style="clear:both;padding:0px;margin-bottom:1rem;">
+				<?php $cid = 0;
+ foreach($this->tpl_var['courses']['data'] as $key => $course){ 
+ $cid++; ?>
+				<div class="col-xs-6" style="padding:3px;">
+					<div style="background-color:#FFFFFF;padding:7px">
+						<a class="ajax" href="index.php?course-phone-course&csid=<?php echo $course['csid']; ?>" data-target="courseplatform" data-page="courseplatform" style="width:8rem;">
+							<img src="<?php echo $course['csthumb']; ?>" style="width:100%;" alt="<?php echo $course['cstitle']; ?>">
+						</a>
+						<h5 class="text-center"><a class="ajax" href="index.php?course-phone-course&csid=<?php echo $course['csid']; ?>" data-target="courseplatform" data-page="courseplatform"><?php echo $course['cstitle']; ?></a></h5>
+					</div>
+				</div>
+				<?php } ?>
+			</div>
+			<?php $cid = 0;
+ foreach($this->tpl_var['contents'] as $key => $contents){ 
+ $cid++; ?>
+			<?php if($key != 2){ ?>
+			<div>
+				<h4 style="overflow:hidden;clear:both;padding-top:0.2rem;">
+					<span class="pull-left" style="width:35%"><hr /></span>
+					<span class="col-xs-4 text-center" style="width:30%;line-height:4rem;"><?php echo $this->G->make('strings')->subString($this->tpl_var['rcats'][$key]['catname'],15); ?></span>
+					<span class="pull-right" style="width:35%"><hr /></span>
+				</h4>
+			</div>
+			<div style="clear:both;padding:1.5rem;background-color:#FFFFFF;margin-bottom:1rem;" class="col-xs-12">
+				<?php $cid = 0;
+ foreach($contents['data'] as $key => $content){ 
+ $cid++; ?>
+				<div class="media">
+					<div class="media-body">
+						<h5 class="media-heading"><a class="ajax" href="index.php?content-phone-content&contentid=<?php echo $content['contentid']; ?>" data-target="contentpage" data-page="contentpage"><?php echo $content['contenttitle']; ?></a></h5>
+						<p style="font-size:0.8rem;"><?php echo $this->G->make('strings')->subString($content['contentdescribe'],81); ?> </p>
+					</div>
+				</div>
+				<?php } ?>
+			</div>
+			<?php } ?>
+			<?php } ?>
+		</div>
+		</div>
+		<script>
+			$(function(){
+				var mySwiper = new Swiper('.swiper-container',{
+					"pagination":".swiper-pagination",
+					"loop":true,
+					"autoplay": 3000,
+					"observer":true,
+					"observeParents":true
+				});
+			});
+		</script>
+		<?php $this->_compileInclude('footer'); ?>
+<?php if(!$this->tpl_var['userhash']){ ?>
+    </div>
+</div>
+<script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
+<script>
+	wx.config({
+        debug: false,
+        appId: '<?php echo $signPackage['appId']; ?>',
+        timestamp: <?php echo $signPackage['timestamp']; ?>,
+		nonceStr: '<?php echo $signPackage['nonceStr']; ?>',
+        signature: '<?php echo $signPackage['signature']; ?>',
+        jsApiList:  ['onMenuShareTimeline','onMenuShareAppMessage']
+    });
+</script>
+</body>
+</html>
+<?php } ?>
---
+++ data/compile/exam/tpls/phone/%%cpl%%questions.php
@@ -0,0 +1,146 @@
+<?php if(!$this->tpl_var['userhash']){ ?>
+<?php $this->_compileInclude('header'); ?>
+<body>
+<div id="content">
+	<div class="pages" id="questions">
+<?php } ?>
+		<div class="pagecontent">
+			<header class="container-fluid" style="background-color:#337AB7;">
+				<h5 class="text-center">
+					<em style="font-size:2rem;" class="pull-left glyphicon glyphicon-chevron-left" onclick="javascript:$.goPrePage();"></em>
+					普通试题
+					<em style="font-size:2rem;" class="pull-right glyphicon glyphicon-home" onclick="javascript:$.goPage('#page1');"></em>
+				</h5>
+			</header>
+			<div class="container-fluid">
+				<div class="row-fluid">
+					<ul class="nav nav-tabs" style="padding-top:1rem;">
+						<li class="active">
+							<a href="index.php?exam-phone-questions" class="ajax">普通试题</a>
+						</li>
+						<li>
+							<a href="index.php?exam-phone-questions-questionrows" class="ajax">题冒题</a>
+						</li>
+					</ul>
+					<form action="index.php?exam-phone-questions" method="post" class="form-inline">
+						<table class="table">
+							<tr>
+								<td>
+									试题ID：
+								</td>
+								<td>
+									<input name="search[questionid]" class="form-control" size="10" type="text" class="number" value="<?php echo $this->tpl_var['search']['questionid']; ?>"/>
+								</td>
+								<td>
+									关键字：
+								</td>
+								<td>
+									<input class="form-control" name="search[keyword]" size="10" type="text" value="<?php echo $this->tpl_var['search']['keyword']; ?>"/>
+								</td>
+							</tr>
+							<tr>
+								<td>
+									试题类型：
+								</td>
+								<td>
+									<select name="search[questiontype]" class="form-control">
+										<option value="0">类型不限</option>
+										<?php $qid = 0;
+ foreach($this->tpl_var['questypes'] as $key => $questype){ 
+ $qid++; ?>
+										<option value="<?php echo $questype['questid']; ?>"<?php if($this->tpl_var['search']['questiontype'] == $questype['questid']){ ?> selected<?php } ?>><?php echo $questype['questype']; ?></option>
+										<?php } ?>
+									</select>
+								</td>
+
+								<td>
+									难度：
+								</td>
+								<td>
+									<select name="search[questionlevel]" class="form-control">
+										<option value="0">难度不限</option>
+										<option value="1"<?php if($this->tpl_var['search']['questionlevel'] == 1){ ?> selected<?php } ?>>易</option>
+										<option value="2"<?php if($this->tpl_var['search']['questionlevel'] == 2){ ?> selected<?php } ?>>中</option>
+										<option value="3"<?php if($this->tpl_var['search']['questionlevel'] == 3){ ?> selected<?php } ?>>难</option>
+									</select>
+								</td>
+							</tr>
+							<tr>
+								<td>
+									章节：
+								</td>
+								<td>
+									<select name="search[questionsectionid]" class="combox form-control" id="sectionselect" target="knowsselect" refUrl="index.php?exam-app-questions-getknowsbysectionid&sectionid={value}">
+									<option value="0">选择章节</option>
+									<?php if($this->tpl_var['sections']){ ?>
+									<?php $sid = 0;
+ foreach($this->tpl_var['sections'] as $key => $section){ 
+ $sid++; ?>
+									<option value="<?php echo $section['sectionid']; ?>"<?php if($section['sectionid'] == $this->tpl_var['search']['questionsectionid']){ ?> selected<?php } ?>><?php echo $section['section']; ?></option>
+									<?php } ?>
+									<?php } ?>
+									</select>
+								</td>
+								<td>
+									知识点：
+								</td>
+								<td>
+									<select name="search[questionknowsid]" id="knowsselect" class="form-control">
+										<option value="">选择知识点</option>
+										<?php if($this->tpl_var['knows']){ ?>
+										<?php $kid = 0;
+ foreach($this->tpl_var['knows'] as $key => $know){ 
+ $kid++; ?>
+										<option value="<?php echo $know['knowsid']; ?>"<?php if($know['knowsid'] == $this->tpl_var['search']['questionknowsid']){ ?> selected<?php } ?>><?php echo $know['knows']; ?></option>
+										<?php } ?>
+										<?php } ?>
+									</select>
+								</td>
+							</tr>
+							<tr>
+								<td colspan="2">
+									<button class="btn btn-primary" type="submit">搜索</button>
+									<input type="hidden" value="1" name="search[argsmodel]" />
+								</td>
+								<td colspan="4"></td>
+							</tr>
+						</table>
+					</form>
+					<table class="table table-hover table-bordered">
+						<thead>
+							<tr class="info">
+								<th style="width:7rem;">试题类型</th>
+								<th>试题内容</th>
+								<th style="width:4rem;">难度</th>
+							</tr>
+						</thead>
+						<tbody>
+							<?php $qid = 0;
+ foreach($this->tpl_var['questions']['data'] as $key => $question){ 
+ $qid++; ?>
+							<tr>
+								<td>
+									<?php echo $this->tpl_var['questypes'][$question['questiontype']]['questype']; ?>
+								</td>
+								<td>
+									<a title="查看试题" class="ajax" data-target="questiondetail" data-page="questiondetail" href="index.php?exam-phone-questions-detail&questionid=<?php echo $question['questionid']; ?>"><?php echo $this->G->make('strings')->subString(strip_tags(html_entity_decode($question['question'])),135); ?></a>
+								</td>
+								<td>
+									<?php if($question['questionlevel']==2){ ?>中<?php } elseif($question['questionlevel']==3){ ?>难<?php } elseif($question['questionlevel']==1){ ?>易<?php } ?>
+								</td>
+							</tr>
+							<?php } ?>
+						</tbody>
+					</table>
+					<ul class="pagination pull-right">
+						<?php echo $this->tpl_var['questions']['pages']; ?>
+					</ul>
+				</div>
+			</div>
+		</div>
+	<?php if(!$this->tpl_var['userhash']){ ?>
+    </div>
+</div>
+</body>
+</html>
+<?php } ?>
